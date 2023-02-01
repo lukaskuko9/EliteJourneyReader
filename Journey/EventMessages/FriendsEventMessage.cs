a2ed2d9c6256daade17b8e5b3afa28f77bfa4484
lukaskuko9 <lukaskuko9@gmail.com>
2023-02-01 21:02:03 +0100

    commit

---
+++ Journey/EventMessages/FriendsEventMessage.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace EliteJourneyReader.Journey.EventMessages;
+
+public sealed class FriendsEventMessage : JourneyEventMessage
+{
+    [JsonProperty("status")]
+    public string Status { get; set; }    
+    
+    [JsonProperty("name")]
+    public string FriendName { get; set; }
+}
